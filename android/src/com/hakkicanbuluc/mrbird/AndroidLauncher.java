20f6ca724fcd632d221eae913fe34c90d1269b67
HAKKICAN <hakkicanbuluc@gmail.com>
2021-05-15 11:15:19 +0300

    initial commit

---
+++ android/src/com/hakkicanbuluc/mrbird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hakkicanbuluc.mrbird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hakkicanbuluc.mrbird.MrBirdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MrBirdGame(), config);
+	}
+}
